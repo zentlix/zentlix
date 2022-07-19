68474017241d471f847fd42cae2b5b4c8f537f1f
Maxim Smakouz <m.smakouz@gmail.com>
2022-07-19 21:53:18 +0300

    Remove some assets

--- src/Core/public/admin/js/menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.menu-submit').on('click', function () {
-        $('#main form').submit();
-    });
-    $('.menu-controls').on('click', function (e) {
-        e.preventDefault();
-    });
-});
