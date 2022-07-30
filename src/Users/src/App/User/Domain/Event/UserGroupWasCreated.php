8c9deedeca8c3a9a8c706656187da32e56acffbb
Maxim Smakouz <m.smakouz@gmail.com>
2022-07-30 12:46:26 +0300

    Add AbstractSerializable to UserGroup Event and ReadModel

--- src/Users/src/App/User/Domain/Event/UserGroupWasCreated.php
+++ src/Users/src/App/User/Domain/Event/UserGroupWasCreated.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Zentlix\Users\App\User\Domain\Event;
 
-use Assert\Assertion;
-use Assert\AssertionFailedException;
-use Broadway\Serializer\Serializable;
 use Symfony\Component\Uid\Uuid;
+use Zentlix\Core\App\Shared\Domain\Event\AbstractSerializableEvent;
 use Zentlix\Users\App\User\Domain\Role;
 
-final class UserGroupWasCreated implements Serializable
+final class UserGroupWasCreated extends AbstractSerializableEvent
 {
     public function __construct(
         public Uuid $uuid,
@@ -28,47 +26,5 @@ final class UserGroupWasCreated implements Serializable
 
         public array $rights
     ) {
-    }
-
-    /**
-     * @throws AssertionFailedException
-     */
-    public static function deserialize(array $data): self
-    {
-        Assertion::keyExists($data, 'uuid');
-        Assertion::keyExists($data, 'title');
-        Assertion::keyExists($data, 'code');
-        Assertion::keyExists($data, 'sort');
-        Assertion::keyExists($data, 'role');
-        Assertion::keyExists($data, 'rights');
-
-        Assertion::uuid($data['uuid']);
-        Assertion::string($data['title']);
-        Assertion::string($data['code']);
-        Assertion::integer($data['sort']);
-        Assertion::string($data['role']);
-        Assertion::isArray($data['rights']);
-
-        /** @psalm-suppress ArgumentTypeCoercion */
-        return new self(
-            Uuid::fromString($data['uuid']),
-            $data['title'],
-            $data['code'],
-            $data['sort'],
-            Role::from($data['role']),
-            $data['rights']
-        );
-    }
-
-    public function serialize(): array
-    {
-        return [
-            'uuid' => $this->uuid->toRfc4122(),
-            'title' => $this->title,
-            'code' => $this->code,
-            'sort' => $this->sort,
-            'role' => $this->role->value,
-            'rights' => $this->rights,
-        ];
     }
 }
--- src/Users/src/App/User/Infrastructure/ReadModel/UserGroupView.php
+++ src/Users/src/App/User/Infrastructure/ReadModel/UserGroupView.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Zentlix\Users\App\User\Infrastructure\ReadModel;
 
-use Assert\Assertion;
-use Broadway\ReadModel\SerializableReadModel;
-use Broadway\Serializer\Serializable;
 use OpenApi\Attributes as OA;
 use Symfony\Component\Serializer\Annotation\Ignore;
 use Symfony\Component\Uid\Uuid;
-use Symfony\Component\Uid\UuidV4;
+use Zentlix\Core\App\Shared\Infrastructure\ReadModel\AbstractSerializableReadModel;
 use Zentlix\Users\App\User\Domain\Role;
 
 /**
@@ -22,7 +19,7 @@ use Zentlix\Users\App\User\Domain\Role;
     required: ['uuid', 'title', 'code', 'sort', 'role', 'rights'],
     type: 'object',
 )]
-class UserGroupView implements SerializableReadModel
+class UserGroupView extends AbstractSerializableReadModel
 {
     public const TYPE = 'UserGroupView';
 
@@ -46,45 +43,6 @@ class UserGroupView implements SerializableReadModel
     public Role $role;
 
     public array $rights = [];
-
-    public static function fromSerializable(Serializable $event): self
-    {
-        return self::deserialize($event->serialize());
-    }
-
-    public static function deserialize(array $data): self
-    {
-        Assertion::keyExists($data, 'uuid');
-        Assertion::keyExists($data, 'title');
-        Assertion::keyExists($data, 'code');
-        Assertion::keyExists($data, 'sort');
-        Assertion::keyExists($data, 'role');
-        Assertion::keyExists($data, 'rights');
-
-        $view = new self();
-
-        $view->uuid = UuidV4::fromString($data['uuid']);
-        $view->title = $data['title'];
-        $view->code = $data['code'];
-        /** @psalm-suppress PropertyTypeCoercion sort */
-        $view->sort = (int) $data['sort'];
-        $view->role = Role::from($data['role']);
-        $view->rights = $data['rights'];
-
-        return $view;
-    }
-
-    public function serialize(): array
-    {
-        return [
-            'uuid' => $this->getId(),
-            'title' => $this->title,
-            'code' => $this->code,
-            'sort' => $this->sort,
-            'role' => $this->role->value,
-            'rights' => $this->rights,
-        ];
-    }
 
     #[Ignore]
     public function getId(): string
